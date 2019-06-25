624e3dea237526631d3f3d517a392c0753bf06fa
Grzegorz W <grzegorz.wilczynski@student.put.poznan.pl>
2019-06-25 16:24:46 +0200

    Add project files.

---
+++ Bomberman/Startup.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bomberman
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc();
+
+            ConfigureWebSockets(app);
+        }
+
+        private async Task Echo(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+
+        private void ConfigureWebSockets(IApplicationBuilder app)
+        {
+            app.UseWebSockets();
+
+            var webSocketOptions = new WebSocketOptions()
+            {
+                KeepAliveInterval = TimeSpan.FromSeconds(120),
+                ReceiveBufferSize = 4 * 1024
+            };
+
+            app.UseWebSockets(webSocketOptions);
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/ws")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                        await Echo(context, webSocket);
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+                else
+                {
+                    await next();
+                }
+
+            });
+
+            //app.Use(async (context, next) => {
+            //    var socket = await context.WebSockets.AcceptWebSocketAsync();
+            //    var socketFinishedTcs = new TaskCompletionSource<object>();
+
+            //    BackgroundSocketProcessor.AddSocket(socket, socketFinishedTcs);
+
+            //    await socketFinishedTcs.Task;
+            //});
+        }
+    }
+}
---
+++ Bomberman/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿function init() {
+    websocket = new WebSocket("ws://localhost:50027/ws");
+    websocket.onopen = function (e) { onOpen(e) };
+    websocket.onclose = function (e) { onClose(e) };
+    websocket.onmessage = function (e) { onMessage(e) };
+    websocket.onerror = function (e) { onError(e) };
+}
+
+function onOpen(e) {
+    alert(e.type);
+    websocket.send("Hello");
+}
+
+function onMessage(e) {
+    alert('rcvd: ' + e.data);
+    websocket.close();
+}
+
+window.addEventListener("load", init, false);
