a61cd94083e965b14454026bbf3f1afe88fac3db
Grzegorz W <grzegorz.wilczynski@student.put.poznan.pl>
2019-06-25 17:06:36 +0200

    Szablon obsługi klawiszy + dołączenie jQuery

--- Bomberman/wwwroot/js/site.js
+++ Bomberman/wwwroot/js/site.js
@@ -16,4 +16,49 @@ function onMessage(e) {
     websocket.close();
 }
 
+function spacebarClicked() {
+    alert("spacebarClicked");
+}
+
+function keyUpClicked() {
+    alert("keyUpClicked");
+}
+
+function keyDownClicked() {
+    alert("keyDownClicked");
+}
+
+function keyRightClicked() {
+    alert("keyRightClicked");
+}
+
+function keyLeftClicked() {
+    alert("keyLeftClicked");
+}
+
+function fkey(e) {
+    e = e || window.event;
+
+    switch (e.keyCode) {
+        case 38:
+            keyUpClicked();
+            break;
+        case 40:
+            keyDownClicked();
+            break;
+        case 39:
+            keyRightClicked();
+            break;
+        case 37:
+            keyLeftClicked();
+            break;
+        case 32:
+            spacebarClicked();
+            break;
+        default:
+    }
+}
+
+document.onkeydown = fkey;
+
 window.addEventListener("load", init, false);
